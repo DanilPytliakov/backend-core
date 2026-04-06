d0e65a339cbc5d37467803354dc0d47053ba1dc2
Данил Пытляков <danilpytlikov@gamil.com>
2026-04-06 21:39:40 +0300

    BCORE-32: Liquibase — версионирование базы данных и Product сущность

---
+++ src/main/java/ru/mentee/power/crm/domain/Product.java
@@ -0,0 +1,35 @@
+package ru.mentee.power.crm.domain;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "sku")
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String sku;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+}
