226c2ec44af531950dc07eca60df0c3acace7ce2
Данил Пытляков <danilpytlikov@gamil.com>
2026-04-10 16:41:58 +0300

    BCORE-35: Первый REST контроллер + WireMock интеграционные тесты

---
+++ src/main/java/ru/mentee/power/crm/spring/config/RestTemplateConfig.java
@@ -0,0 +1,28 @@
+package ru.mentee.power.crm.spring.config;
+
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder
+        .connectTimeout(Duration.ofSeconds(5))
+        .readTimeout(Duration.ofSeconds(10))
+        .errorHandler(
+            new DefaultResponseErrorHandler() {
+              @Override
+              protected boolean hasError(HttpStatusCode statusCode) {
+                return statusCode.is4xxClientError() || statusCode.is5xxServerError();
+              }
+            })
+        .build();
+  }
+}
