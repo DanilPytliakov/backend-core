4cb0375c6249911e3888d57ecced41b1e83d771a
Данил Пытляков <danilpytlikov@gamil.com>
2026-04-16 23:55:25 +0300

    вне списка

--- src/main/java/ru/mentee/power/crm/web/rest/config/OpenApiJsonNullableModule.java
+++ src/main/java/ru/mentee/power/crm/web/rest/config/OpenApiJsonNullableModule.java
@@ -5,6 +5,4 @@ import org.springframework.stereotype.Component;
 
 /** Регистрирует поддержку JsonNullable для generated OpenAPI DTO. */
 @Component
-public class OpenApiJsonNullableModule extends JsonNullableModule {
-
-}
+public class OpenApiJsonNullableModule extends JsonNullableModule {}
