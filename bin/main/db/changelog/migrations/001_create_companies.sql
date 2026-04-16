ff660bbae01d4915206e5e1b1212dec2c7903d26
Данил Пытляков <danilpytlikov@gamil.com>
2026-04-16 15:59:53 +0300

    chore: ignore bin and remove tracked build artifacts

--- bin/main/db/changelog/migrations/001_create_companies.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset Danil:BCORE-32-5
-CREATE TABLE IF NOT EXISTS companies
-(
-    id UUID PRIMARY KEY NOT NULL,
-    name   VARCHAR(255)        NOT NULL,
-    industry VARCHAR(100)
-);
